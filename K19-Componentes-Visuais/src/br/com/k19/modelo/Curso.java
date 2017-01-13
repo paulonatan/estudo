d0656225ecae72d2813ddbcb30fa8fc83948ba3b
Paulo Natan <paulonatan41@gmail.com>
2017-01-12 21:30:02 -0300

    Add files via upload

---
+++ K19-Componentes-Visuais/src/br/com/k19/modelo/Curso.java
@@ -0,0 +1,19 @@
+package br.com.k19.modelo;
+
+public class Curso {
+	private String nome;
+	private String sigla;
+	
+	public String getNome(){
+		return this.nome;
+	}
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	public String getSigla(){
+		return this.sigla;
+	}
+	public void setSigla(String sigla){
+		this.sigla = sigla;
+	}
+}
